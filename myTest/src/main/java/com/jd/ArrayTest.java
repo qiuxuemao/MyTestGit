42466c202d634daf55f5591b6a19a7f6a1d09496
qiuxuemao@163.com <qiuxuemao@163.com>
2020-05-23 21:38:27 +0800

    dev

--- myTest/src/main/java/com/jd/ArrayTest.java
+++ myTest/src/main/java/com/jd/ArrayTest.java
@@ -2,6 +2,6 @@ package com.jd;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        System.out.println("this is arrayTest!");
+        System.out.println("this is dev");
     }
 }
